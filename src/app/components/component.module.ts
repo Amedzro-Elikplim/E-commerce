9cb131c55c0dbc667ec97d7211f2c3f090482ed7
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-28 19:05:51 +0000

    Import FormsModule

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from "@angular/core";
 import { LoginComponent } from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
-
+import { FormsModule } from "@angular/forms";
 @NgModule({
     declarations: [
         LoginComponent,
         RegisterComponent
+    ],
+    
+    imports: [
+        FormsModule
     ]
 })
 
