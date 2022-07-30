ad79f18d63ded66ab1122870eb2396d52d05bbf6
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-30 17:47:38 +0000

    Import routing module

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormFieldComponent } from './auth/auth-form-field/form-field/form-field.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, FormFieldComponent],
+  declarations: [LoginComponent, RegisterComponent],
 
   imports: [
     FormsModule,
@@ -18,6 +18,12 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatIconModule,
     MatInputModule,
     MatCheckboxModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
 })
-export class ComponentModule {}
+export class ComponentModule {
+
+
+
+}
