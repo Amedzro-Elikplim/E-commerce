0fe264fd8433fc075f63e82fa3e77a5067e4d018
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-30 08:54:19 +0000

    Import MatButtonModule

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from "@angular/core";
-import { LoginComponent } from "./auth/login/login.component";
-import { RegisterComponent } from "./auth/register/register.component";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormFieldComponent } from './auth/auth-form-field/form-field/form-field.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
-    declarations: [
-        LoginComponent,
-        RegisterComponent
-    ],
-    
-    imports: [
-        FormsModule
-    ]
-})
+  declarations: [LoginComponent, RegisterComponent, FormFieldComponent],
 
-export class ComponentModule { }
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatCheckboxModule,
+  ],
+})
+export class ComponentModule {}
