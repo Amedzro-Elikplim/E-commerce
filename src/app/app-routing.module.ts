658b344b50d5bc5c8d59c19b64a04087cc691d2b
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-31 11:17:46 +0000

    Configure home and checkout page routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
-
+import { HomepageComponent } from './components/pages/homepage/homepage.component';
+import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '', component: HomepageComponent },
+  {path: 'checkout', component: CheckoutPageComponent}
 ];
 
 @NgModule({
